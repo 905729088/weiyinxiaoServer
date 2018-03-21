9129e52c30889f08c97376a6d3e09194e34e5f4c
lufei <905729088@qq.com>
2018-03-21 09:15:05 +0800

    3-21新增notificationwebsocket接口

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,23 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 let phoneList = [];
-//
-// router.get('/phoneIp', function(req, res, next) {
-//     // mobileIp.push(req.query.ip)
-//     res.send(req.query);
-//     console.log("收到" + req.query.id);
-// })
 
+for (let i = 0; i < 30; i++) {
+    const nowData = new Date();
+    const ipaddr = {
+        ip: 123,
+        map: 12312,
+        mode: 12312,
+        phoneNuber: 2131231, //手机号码
+        taskCount: 12312 || 0, //任务个数
+        addCount: 12312 || 0, //添加次数
+        weChatId: 312312, //微信号
+        lastTime: new Date().getTime()
+    }
+    phoneList.push(ipaddr);
+}
+
+//app端接口
 router.post('/ipaddr', function(req, res, next) {
     console.log("ipadder收到");
     res.send({ code: 0, messmage: "succsess" });
@@ -35,6 +45,28 @@ router.post('/ipaddr', function(req, res, next) {
     }
 
 });
+
+router.post('/notificationwebsocket', function(req, res, next) {
+    console.log("inotificationwebsocket收到");
+    res.send({ code: 0, messmage: "succsess" });
+
+});
+
+
+
+//checkPhoneList();
+async function checkPhoneList() {
+    let arr = [];
+    let newT = new Date().getTime();
+    for (let i = 0; i < phoneList.length; i++) {
+        if (newT - phoneList[i].lastTime < 10) {
+
+        }
+
+    }
+}
+
+//前端接口
 router.post('/phone', function(req, res, next) {
     console.log("Phone收到");
     res.send(phoneList);
